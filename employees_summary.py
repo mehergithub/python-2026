c9fffa1ea81c2fbd61b95ff1f19e03a87cfc1dc5
Meher <82973618+mehergithub@users.noreply.github.com>
2026-03-03 17:51:54 -0500

    Add files via upload

---
+++ employees_summary.py
@@ -0,0 +1,3 @@
+[{"id": 1, "name": "Andy Roberts", "job": "SP", "salary": 1500000},
+  {"id": 2, "name": "Larry Ellison", "job": "PL", "salary": 3500000},
+  {"id": 3, "name": "James Gosling", "job": "TL", "salary": 2500000}]
---
+++ pos_only_args.py
@@ -0,0 +1,13 @@
+def mathoperation(n1, n2, task):
+    print(task(n1, n2))
+
+def add(n1, n2):
+    return n1 + n2
+
+def square(n):
+    return n * n
+
+mathoperation(10, 20, add)
+#mathoperation(10, 20, square)
+
+mathoperation(10, 5, lambda a,b : a ** b)
